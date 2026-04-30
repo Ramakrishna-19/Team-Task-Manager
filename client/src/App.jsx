8968cfffa464e2db9b2aadd396d8951683757fa0
Ramakrishna <ramakrishnadyavari1920@gmail.com>
2026-05-01 04:48:45 +0530

    test app render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Projects from "./pages/Projects";
-import Tasks from "./pages/Tasks";
-import MakeAdmin from "./pages/MakeAdmin";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Dashboard from "./pages/Dashboard";
+// import Projects from "./pages/Projects";
+// import Tasks from "./pages/Tasks";
+// import MakeAdmin from "./pages/MakeAdmin";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Register />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/projects" element={<Projects />} />
+//         <Route path="/projects/:id" element={<Tasks />} />
+//         <Route path="/make-admin" element={<MakeAdmin />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:id" element={<Tasks />} />
-        <Route path="/make-admin" element={<MakeAdmin />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1 style={{ color: "black" }}>APP WORKING</h1>;
 }
